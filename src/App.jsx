4ac9faa3ffc54dc830a2c5812bad74704c950bce
jessy <rugurujane1@gmail.com>
2025-02-16 22:02:02 +0300

    Fix duplicate Router issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { Routes, Route, useLocation } from "react-router-dom";
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom"; // Only use Routes, not Router
 import Header from "./components/Header";
 import CartOverlay from "./components/CartOverlay";
 import { CartProvider } from "./context/CartContext";
@@ -9,12 +9,6 @@ import "./App.css";
 
 const App = () => {
   const [isOverlayVisible, setIsOverlayVisible] = useState(false);
-  const location = useLocation();
-
-  useEffect(() => {
-    console.log("Current Route:", location.pathname);
-  }, [location]);
-
   const toggleOverlay = () => setIsOverlayVisible((prev) => !prev);
 
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
-// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ApolloProvider } from "@apollo/client";
-import { HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; // Keep HashRouter ONLY here
 import App from "./App";
 import client from "./graphql/apolloClient";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ApolloProvider client={client}>
-    <HashRouter>
+    <HashRouter> {/* This should be the only Router */}
       <App />
     </HashRouter>
   </ApolloProvider>
