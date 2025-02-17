360d30248f6f3ac61afb7f16a9dccd5860e2b73f
jessy <rugurujane1@gmail.com>
2025-02-17 21:04:38 +0300

    Describe the changes made to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import CartOverlay from "./components/CartOverlay";
 import { CartProvider } from "./context/CartContext";
@@ -13,19 +13,21 @@ const App = () => {
 
   return (
     <CartProvider>
-      <Header toggleOverlay={toggleOverlay} />
-      {isOverlayVisible && <CartOverlay onClose={toggleOverlay} />}
-      <Routes>
-        <Route path="/" element={<ProductListingPage />} />
-        <Route path="/all" element={<ProductListingPage />} />
-        <Route path="/tech" element={<ProductListingPage />} />
-        <Route path="/clothes" element={<ProductListingPage />} />
-        <Route
-          path="/product/:id"
-          element={<ProductDetailsPage toggleOverlay={toggleOverlay} />}
-        />
-        <Route path="*" element={<ProductListingPage />} />
-      </Routes>
+      <BrowserRouter basename={window.location.pathname.split('/')[1] || ''}>
+        <Header toggleOverlay={toggleOverlay} />
+        {isOverlayVisible && <CartOverlay onClose={toggleOverlay} />}
+        <Routes>
+          <Route path="/" element={<ProductListingPage />} />
+          <Route path="/all" element={<ProductListingPage />} />
+          <Route path="/tech" element={<ProductListingPage />} />
+          <Route path="/clothes" element={<ProductListingPage />} />
+          <Route
+            path="/product/:id"
+            element={<ProductDetailsPage toggleOverlay={toggleOverlay} />}
+          />
+          <Route path="*" element={<ProductListingPage />} />
+        </Routes>
+      </BrowserRouter>
     </CartProvider>
   );
 };
